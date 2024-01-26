4f8bb19ee110fded2dbd10a5ef238d6d3153bc0b
“Brenno <brennoeudes@gmail.com>
2024-01-26 10:59:35 -0300

    js logic correction

--- main.js
+++ main.js
@@ -5,23 +5,33 @@ const screen2 = document.querySelector(".screen-2");
 const cookieBtn = document.querySelector("#btn-cookie");
 const anotherCookieBtn = document.querySelector("#btn-another-cookie");
 
+const fortuneMessage = document.querySelector("#fortune-message");
+const fortuneMessagesData = ["teste", "teste2", "teste3"];
+
 // Eventos:
+fortuneMessage.addEventListener("click", handleCookieClick);
 cookieBtn.addEventListener("click", handleCookieClick);
 anotherCookieBtn.addEventListener("click", handleAnotherCookieClick);
 
 // Funções:
 function handleCookieClick(e) {
-    console.log("Clicou no 1º btn!");
   toggleScreen(screen2, screen1);
+  pickMessage();
 }
 
 function handleAnotherCookieClick(e) {
-    console.log("Clicou no 2º btn!");
   toggleScreen(screen1, screen2);
 }
 
+function pickMessage() {
+  let allMessages = fortuneMessagesData.length;
+  console.log(allMessages);
+  let randomNumber = Math.floor(Math.random() * 3);
+  console.log(randomNumber);
+  screen2.querySelector("h2").innerText = `${fortuneMessagesData[randomNumber]}`;
+}
+
 function toggleScreen(showScreen, hideScreen) {
-    console.log("Inverteu as classes!");
   showScreen.classList.remove("hide");
   hideScreen.classList.add("hide");
 }
